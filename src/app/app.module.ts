a690a3881d5ae048847b8b9711e3cdcb3369609d
vinay.limaye@craftedbyredcedar.com <vinay.limaye@craftedbyredcedar.com>
2024-03-19 12:23:15 +0530

    Initial Merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormQuestionComponent } from './form-question/form-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormQuestionComponent
   ],
   imports: [
     BrowserModule,
